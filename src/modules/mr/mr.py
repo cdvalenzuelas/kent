6071057090e9b67a486be2321ca9abf8fee3dfda
cdvalenzuelas <cdvalenzuelas@gmail.com>
2022-06-08 23:51:57 -0500

    FIX MR

--- src/modules/mr/mr.py
+++ src/modules/mr/mr.py
@@ -8,5 +8,6 @@ def mr():
     # Filtrar el archivo y dejar sólo las válvulas
     mr_df = mr_df[mr_df['TYPE'] == 'VL']
 
-    # Crear el archivo summary
-    mr_df.to_csv('./output/mr.csv', index=True)
+    if mr_df.shape[0] > 0:
+        # Crear el archivo summary
+        mr_df.to_csv('./output/mr.csv', index=True)
